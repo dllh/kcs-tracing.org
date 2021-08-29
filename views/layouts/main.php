036b6852f7388e5f782f4dd2d44d84bcdeb2b644
Daryl Houston <daryl@learnhouston.com>
2021-08-29 04:18:30 +0000

    Update main footer.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -72,8 +72,7 @@ AppAsset::register($this);
 
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
-        <p class="float-left">&copy; My Company <?= date('Y') ?></p>
-        <p class="float-right"><?= Yii::powered() ?></p>
+        <p class="float-left">&copy; @dllh <?= date('Y') ?></p>
     </div>
 </footer>
 
