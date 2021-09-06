c0b6cd9260eb8ddb0019a54941fccf41b6fb734c
Daryl Houston <daryl@learnhouston.com>
2021-09-06 02:47:25 +0000

    Add grade-level constraints dependent upon school selected, so that e.g. if you pick a high school from the school list, it's not possible to select kindergarten or some other non-high-school grade when submitting a report. Fixes #3.

--- models/School.php
+++ models/School.php
@@ -20,6 +20,36 @@ class School extends ActiveRecord {
 		];
 	}
 
+	/*
+	 * Different school types support different grade levels. E.g. most high schoolsare grades 9 - 12.
+	 * In order to enforce correct grade levels per school, we've classified each set of grade levels
+	 * as given in thearray below. We've also added a column to the schools table whose values 
+	 * correspond with the array keys below. In the report/save view, we do some fancy javascript
+	 * in order to cause selection of a school to limit the grade select box options to valid
+	 * subsets of the grades as given below.
+	 *
+	 * If ever we need to update the grade mappings, we may need to add more types here and update the
+	 * schools table's "type" column to match the relevant column.
+	 */
+	public function getValidGrades( $type = null ) {
+		$map = [
+			'high'         => [ '9', '10', '11', '12' ],
+			'elementary'   => [ 'K', '1', '2', '3', '4', '5' ],
+			'intermediate' => [ 'K', '1', '2', '3', '4', '5', '6' ],
+			'primary'      => [ 'K', '1', '2', '3', '4', '5', '6' ],
+			'middle'       => [ '6', '7', '8' ],
+			'unspecified'  => [ 'Pre-K', 'Kindergarten', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12' ],
+		];
+
+		if ( 'map' == $type ) {
+			return $map;
+		} else if ( ! is_null( $type ) && array_key_exists( $type, $map ) ) {
+			return $map[ $type ];
+		} else {
+			return $map[ $this->type ];
+		}
+	}
+
 	public function getBoardMember() {
                 return $this->hasOne( BoardMember::class, [ 'id' => 'board_member_id' ] );
 	}
--- views/reports/save.php
+++ views/reports/save.php
@@ -5,23 +5,76 @@ use yii\jui\DatePicker;
 use app\models\School;
 use app\models\Report;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
 use kartik\select2\Select2;
 
+/*
+ * The code just below is used for some logic that lets us map grade levels to
+ * school by classifying each school via its "type" column as a type of school ("high,"
+ * "elementary," etc.). We use those classifications in the Schools model and in
+ * the select2 js events below within this file to do some magic that causes
+ * grades to be limited to the subset of _valid_ grades for the selected school.
+ */
+$schools = School::find()->orderBy( 'name' )->all();
+$schoolData = ArrayHelper::map( $schools, 'id', 'name' );
+
+$validGrades = School::getValidGrades( 'map' );
+$schoolToTypeMap = [];
+
+foreach ( $schools as $school ) {
+	$schoolToTypeMap[ $school->id ] = $school->type;
+}
 ?>
 
 <h1>Add Report</h1>
 <p>Please fill out this form only <b>once per child per infection</b>. We'd like to know both when your child first demonstrated symptoms and when your child had a positive test date.</p>
 
+<script type="text/javascript">
+<?php 
+	echo 'var schoolToTypeMap = ' . Json::encode( $schoolToTypeMap ) . ";\n"; 
+	echo 'var validGradesMap = ' . Json::encode( $validGrades ) . ";\n"; 
+?>
+</script>
+
+
 <?php
 
-$schoolData = ArrayHelper::map( School::find()->orderBy( 'name' )-> all(), 'id', 'name' );
+
 $form = ActiveForm::begin([
     'id' => 'report-form',
     'options' => ['class' => 'form-vertical'],
 ]) ?>
 
-    <?= $form->field($model, 'school_id')->widget( Select2::classname(), [ 'language' => 'en', 'options' => [ 'placeholder' => 'Select a school...' ], 'pluginOptions' => [ 'allowClear' => true ], 'data' => $schoolData ] )->label( 'Student\'s School' ); ?>
-    <?= $form->field($model, 'grade')->widget( Select2::classname(), [ 'language' => 'en', 'options' => [ 'placeholder' => 'Select a grade...' ], 'pluginOptions' => [ 'allowClear' => true ], 'data' => [ 'Pre-K' => 'Pre-K', 'K' => 'K', '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, '10' => 10, '11' => 11, '12' => 12 ] ] )->label( 'Student\'s Grade Level' ); ?>
+    <?= $form->field($model, 'school_id')
+	     ->widget( Select2::classname(), 
+	     [ 'language' => 'en', 
+	    	 'options' => [ 
+			 'placeholder' => 'Select a school...' 
+		 ], 
+		'pluginOptions' => [ 
+			'allowClear' => true 
+		], 
+		'pluginEvents' => [
+			'change' => 'function( event ) { 
+    				var options = [];
+    				var school_id = $( "#report-school_id" ).val();
+				var gradeMap = validGradesMap[ schoolToTypeMap[ school_id ] ]; 
+
+				$( "#report-grade option" ).remove();
+				$.each( gradeMap, function( idx, grade ) {
+					var newOption = new Option( grade, grade, false, false);
+					$("#report-grade" ).append( newOption ).trigger( "change" );
+				} );
+
+			 }'
+		],
+		'data' => $schoolData,
+	     ]
+	     )->label( 'Student\'s School' ); 
+    ?>
+
+
+    <?= $form->field($model, 'grade')->widget( Select2::classname(), [ 'language' => 'en', 'options' => [ 'placeholder' => 'Select a grade level...' ], 'pluginOptions' => [ 'allowClear' => true ], 'data' => [ '11' => 11, '12' => 12 ] ] )->label( 'Student\'s Grade Level' ); ?>
     <?= $form->field($model, 'symptomatic_date')->widget(\yii\jui\DatePicker::classname(), [ 'dateFormat' => 'yyyy-MM-dd', ] )->label( 'Symptom Onset Date' ) ?>
     <?= $form->field($model, 'positive_test_date')->widget(\yii\jui\DatePicker::classname(), [ 'dateFormat' => 'yyyy-MM-dd', ] )->label( 'Date of Positive Test (when was the test <b>given</b>?)' ) ?>
 
